ba299ba67e909d8c75cae69a89aae37b367f71eb
ArtSv <ArtSv@DESKTOP-JK44D29>
2024-10-10 15:31:45 +0300

    Добавьте файлы проекта.

---
+++ project 4/project 4.cpp
@@ -0,0 +1,32 @@
+﻿// project 4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    class Triangle {
+    public:
+        int height;
+        int length;
+        string info;
+
+        Triangle(int h, int l) {
+            height = h;
+            length = l;
+            info = "Площадь прямоугольника: ";
+        }
+        void setInfo(string s) {
+            info = s;
+        }
+    };
+    
+    Triangle myTriangle(5, 7);
+    cout << myTriangle.info << myTriangle.length*myTriangle.height << endl;
+    return 0;
+}
+
+
